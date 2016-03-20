00cd802c3990d0efb334cf766c354e6316a09679
William Yao <williamyaoh@gmail.com>
2016-03-20 17:08:08 -0500

    moar keybinds

--- keybindings.el
+++ keybindings.el
@@ -8,18 +8,32 @@
 
 (defvar dyntu-global-map (make-keymap))
 
-(defun bind (k f) (define-key dyntu-global-map (kbd k) f))
+(defun bind (k f)
+  (if (fboundp f)
+      (define-key dyntu-global-map (kbd k) f)
+    (warn "Function `%s' is not bound. Could not bind %s."
+	  f
+	  k)))
 
 ;;; First, we set all printing keys to `self-insert-command'.
 (loop for b from 32 upto 126
       do (bind (byte-to-string b) 'self-insert-command))
 
-;;; Basic editing.
+;;; Fundamental stuff.
+(bind "M-x" 'execute-extended-command)
+(bind "C-g" 'keyboard-quit)
+
+(bind "C-x C-f" 'find-file)
+(bind "C-x b" 'switch-to-buffer)
+
+(bind "C-x C-c" 'save-buffers-kill-terminal)
+
+;;; Editing.
 (bind "C-u" 'universal-argument)
 
-(define-key dyntu-global-map (kbd "<backspace>") 'backward-delete-char)
-(define-key dyntu-global-map (kbd "RET") 'newline-and-indent)
-(define-key dyntu-global-map (kbd "C-j") 'newline)
+(bind "<backspace>" 'backward-delete-char)
+(bind "RET" 'newline-and-indent)
+(bind "C-j" 'newline)
 
 (defun open-insert-line (p)
   (interactive "P")
@@ -32,20 +46,47 @@
 
 (bind "C-o" 'open-insert-line)
 
-;;; Basic movement.
+(bind "C-w" 'backward-kill-word)
+(bind "C-x C-k" 'kill-region)
+
+(bind "C-k" 'kill-line)
+
+(bind "C-x j" 'delete-indentation)
+
+(bind "C-n" 'undo)
+
+(bind "C-x TAB" 'indent-rigidly)
+(bind "C-." 'indent-relative)
+
+(bind "C-;" 'comment-or-uncomment-region)
+
+;;; Movement.
 (setf next-line-add-newlines nil)
 (setf line-move-visual t)
 
-(define-key dyntu-global-map (kbd "C-M-h") 'backward-char)
+(bind "C-M-h" 'backward-char)
 (bind "<left>" 'backward-char)
-(define-key dyntu-global-map (kbd "C-M-t") 'next-line)
+(bind "C-M-t" 'next-line)
 (bind "<down>" 'next-line)
-(define-key dyntu-global-map (kbd "C-M-n") 'previous-line)
+(bind "C-M-n" 'previous-line)
 (bind "<up>" 'previous-line)
-(define-key dyntu-global-map (kbd "C-M-s") 'forward-char)
+(bind "C-M-s" 'forward-char)
 (bind "<right>" 'forward-char)
+
+(bind "C-x M-h" 'windmove-left)
+(bind "C-x M-t" 'windmove-down)
+(bind "C-x M-n" 'windmove-up)
+(bind "C-x M-s" 'windmove-right)
+
+(bind "C-M-," 'beginning-of-buffer)
+(bind "C-M-." 'beginning-of-buffer)
 
 (bind "C-a" 'move-beginning-of-line)
 (bind "C-e" 'move-end-of-line)
+
+(bind "C-s" 'isearch-forward)
+(bind "C-r" 'isearch-backward)
+
+(bind "C-l" 'recenter-top-bottom)
 
 (use-global-map dyntu-global-map)
