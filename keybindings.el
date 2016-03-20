2985a18015af9e683570a19e3bb5e2b6ec25b4e3
William Yao <williamyaoh@gmail.com>
2016-03-20 01:37:00 -0500

    more keybinding stuff

--- keybindings.el
+++ keybindings.el
@@ -4,6 +4,15 @@
 ;;;;      the specific customization for that mode
 ;;;;   b) are easy to use on a *Dvorak* keyboard
 
+(require 'cl)
+
 (defvar dyntu-global-map (make-keymap))
+
+;;; First, we set all keys to `self-insert-command'.
+(define-key dyntu-global-map [t] 'self-insert-command)
+
+(define-key dyntu-global-map (kbd "<backspace>") 'backward-delete-char)
+(define-key dyntu-global-map (kbd "RET") 'newline-and-indent)
+(define-key dyntu-global-map (kbd "C-j") 'newline)
 
 (use-global-map dyntu-global-map)
