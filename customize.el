e9ae0f49dc2d66beff1e0b00495abb9d1907cde5
William Yao <williamyaoh@gmail.com>
2016-03-22 14:38:40 -0500

    remove modeline box

--- customize.el
+++ customize.el
@@ -12,8 +12,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "light gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Droid Sans Mono"))))
- '(mode-line ((t (:background "#505050" :foreground "white"))))
- '(powerline-active1 ((t (:background "#303030" :foreground "#e0e0e0"))))
+ '(mode-line ((t (:background "#505050" :foreground "white" :box nil))))
+ '(powerline-active1 ((t (:background "#303030" :foregroun d "#e0e0e0"))))
  '(powerline-active2 ((t (:background "#101010" :foreground "#cfcfcf"))))
  '(powerline-inactive1 ((t (:background "#404040" :foreground "#e0e0e0"))))
  '(powerline-inactive2 ((t (:background "#1f1f1f" :foreground "#cfcfcf"))))
