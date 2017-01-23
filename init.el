4f1301ddbf80ddcea6dc3dac43b6162ee1b3bacd
William Yao <williamyaoh@gmail.com>
2017-01-23 03:52:32 -0600

    typo ww

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 (load-library "~/.emacs.d/setup-cask.el")
 
 ;;;; - Keybindings -------------------------------------------------------------
-;;;                 Edit `keybindsings.el' to change global keybinds.
+;;;                 Edit `keybindings.el' to change global keybinds.
 
 (load-library "~/.emacs.d/keybindings.el")
 ;;;; ---------------------------------------------------------------------------
