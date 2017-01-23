144d1a84c090167daeb35faeddc9073986f1f23d
William Yao <williamyaoh@gmail.com>
2017-01-23 05:00:20 -0600

    font

--- customize.el
+++ customize.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "light gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Droid Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "light gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "Anonymous Pro"))))
  '(mode-line ((t (:background "#505050" :foreground "white" :box nil))))
  '(mode-line-inactive ((t (:box nil))))
  '(parenthesis ((t (:foreground "gray60"))))
