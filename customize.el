6c8a3ca8735002429e6d31368c11f0e37a60442c
William Yao <williamyaoh@gmail.com>
2017-01-24 22:05:41 -0600

    keywords and stuff

--- customize.el
+++ customize.el
@@ -29,7 +29,7 @@
  '(font-lock-type-face ((t nil)))
  '(font-lock-keyword-face ((t nil)))
  '(font-lock-function-name-face ((t nil)))
- '(font-lock-builtin-face ((t (:foreground "#891f00"))))
+ '(font-lock-builtin-face ((t (:foreground "#d38974"))))
  '(font-lock-comment-face ((t (:foreground "#bca753"))))
  '(cursor ((t (:background "deep pink"))))
  '(show-paren-match ((t (:background "gold2")))))
