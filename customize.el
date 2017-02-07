42fdeca24b290e3eca502a76c91c94f00dd93829
William Yao <williamyaoh@gmail.com>
2017-02-07 09:18:07 -0600

    change font

--- customize.el
+++ customize.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "light gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "Anonymous Pro"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "light gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Source Code Pro"))))
  '(mode-line ((t (:background "#c13019" :foreground "white" :box nil))))
  '(mode-line-inactive ((t (:box nil))))
  '(parenthesis ((t (:foreground "gray40"))))
